b162d5e640a42714627300f518a9e9199b200d47
NgoThuyLinh <ngothuylinh.sweet.97@gmail.com>
2019-10-17 17:40:16 +0700

    Add project files.

---
+++ ShopGame.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopGame.Model
+{
+    public class Class1
+    {
+    }
+}
